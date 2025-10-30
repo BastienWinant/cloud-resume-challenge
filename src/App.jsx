2dbab290d0345c2939dc2c9a27b06c033379f5e5
BastienWinant <bastien.winant@gmail.com>
2025-10-30 21:21:07 +0100

    landing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import {Button} from "@chakra-ui/react"
+import { ScrollArea } from "@chakra-ui/react"
+import Landing from "@/components/Landing.jsx"
 import './App.css'
 
 function App() {
   return (
-    <Button>Button</Button>
+		<ScrollArea.Root size="xs" height="100vh" variant="hover">
+			<ScrollArea.Viewport>
+				<ScrollArea.Content>
+					<Landing />
+				</ScrollArea.Content>
+			</ScrollArea.Viewport>
+			<ScrollArea.Scrollbar>
+				<ScrollArea.Thumb />
+			</ScrollArea.Scrollbar>
+			<ScrollArea.Corner />
+		</ScrollArea.Root>
   )
 }
 
---
+++ src/components/Landing.jsx
@@ -0,0 +1,24 @@
+import { Center, ButtonGroup, Button, Heading, Highlight, VStack, Text } from "@chakra-ui/react"
+
+
+export default function Landing() {
+	return (
+		<Center h="100vh" maxH="700px">
+			<VStack w="11/12" maxW="2xl" textAlign="center" gap="6">
+				<Heading size="lg">
+					<Highlight ignoreCase query="bastien winant" styles={{ color: "teal.600" }}>
+						Hi, I'm Bastien Winant
+					</Highlight>
+				</Heading>
+				<Text mb="3" fontSize="md" color="fg.muted">
+					Passionate about building clean, user-friendly web applications.
+					I love turning ideas into reality through code and creating experiences that make a difference.
+				</Text>
+				<ButtonGroup>
+					<Button variant="solid" size="sm" w="42">view my work</Button>
+					<Button variant="outline" size="sm" w="42">get in touch</Button>
+				</ButtonGroup>
+			</VStack>
+		</Center>
+	)
+}
