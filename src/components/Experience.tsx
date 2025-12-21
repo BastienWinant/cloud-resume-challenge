735a786ad379baaa0c8113bc09553a120f263750
BastienWinant <bastien.winant@gmail.com>
2025-12-21 23:37:40 +0100

    add resume entries

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -10,18 +10,17 @@ type ExperienceProps = {
 
 export default function Experience({title, company, dates, location, children}:ExperienceProps) {
 	return (
-		<article className="flex flex-col">
+		<article className="pt-4 flex flex-col gap-8 border-t border-t-secondary/20">
 			<header>
-				{/*TITLE*/}
 				<h3 className="flex flex-col gap-1 capitalize font-medium mb-2">{title}</h3>
 				<ul className="text-secondary">
 					<li>{company}</li>
 					<li className="capitalize">{dates}</li>
-					<li>{location}</li> {/*location*/}
+					<li>{location}</li>
 				</ul>
 			</header>
 			<div>
-				<ul>
+				<ul className="flex flex-col gap-3 text-sm list-disc">
 					{children}
 				</ul>
 			</div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,33 +1,47 @@
+import Experience from "../components/Experience"
+
 export default function Index() {
 	return (
-		<section className="border border-red-600">
+		<section>
 			<h2 className="text-xs font-semibold capitalize">experience</h2>
 
-			<div className="flex flex-col">
-				<article className="flex flex-col">
-					<header>
-						{/*TITLE*/}
-						<h3 className="flex flex-col gap-1 capitalize font-medium mb-2">senior producer</h3>
-						<ul className="text-secondary">
-							<li>Creative Agency XYZ</li> {/*company*/}
-							<li>June 2022 - Present</li> {/*dates*/}
-							<li>Remote & New York, NY</li> {/*location*/}
-						</ul>
-					</header>
-					<div>
-						<ul className="flex flex-col gap-2 text-sm">
-							<li>
-								Seamlessly facilitate diverse creative experiences: from immersive installations to cinematic narratives. A catalyst for collaboration across all mediums.
-							</li>
-							<li>
-								Clients include: Lunethra, Driftwell, Clyra, Forgekind and more.
-							</li>
-						</ul>
-					</div>
-				</article>
-				<article>
-					this is the next
-				</article>
+			<div className="flex flex-col gap-12">
+				<Experience
+					title="senior producer"
+					company="Creative Agency XYZ"
+					dates="June 2022 - Present"
+					location="Remote & New York, NY"
+				>
+					<li>
+						Seamlessly facilitate diverse creative experiences: from immersive installations to cinematic narratives.
+						A catalyst for collaboration across all mediums.
+					</li>
+					<li>
+						Clients include: Lunethra, Driftwell, Clyra, Forgekind and more.
+					</li>
+				</Experience>
+				<Experience
+					title="senior producer"
+					company="Pixel Studios"
+					dates="December 2021 - June 2022"
+					location="New York, NY"
+				>
+					<li>
+						Orchestrated the creation of integrated video, motion, and photo content for multi-platform marketing
+						campaigns, ensuring seamless execution and brand alignment.
+					</li>
+					<li>
+						Directed all phases of production operations (pre-production, production, post-production),
+						meticulously managing budgets, contracts, and legal requirements to deliver projects on time and within quality standards.
+					</li>
+					<li>
+						Strategically developed and managed production schedules, creative strategies, and talent relations,
+						optimizing resources to deliver high-quality content within budget parameters.
+					</li>
+					<li>
+						Clients include: Nestive, Zentrox, Pollenate, Vireo and more.
+					</li>
+				</Experience>
 			</div>
 		</section>
 	)
