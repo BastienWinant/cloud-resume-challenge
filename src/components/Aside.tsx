59934c3bf2e034ec150863d48240cea7fcfcf5dd
BastienWinant <bastien.winant@gmail.com>
2026-01-23 00:16:38 +0100

    minor changes

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,16 +1,16 @@
 export default function Aside() {
 	return (
-		<aside className="self-start md:sticky md:top-0 mx-3 py-7 flex flex-col gap-y-24 border-t-2 border-zinc-100/20">
+		<aside className="self-start md:sticky md:top-0 mx-3 py-7 flex flex-col gap-y-24 border-t-2 border-zinc-100/10">
 			<p className="w-[85%] lg:w-[80%] max-w-96 text-sm lg:text-base">
 				A seasoned Senior Producer with 10+ years of experience,
 				I excel in leading complex marketing and design projects from concept to completion.
 			</p>
 
 			<nav className="flex flex-col text-xs">
-				<a className="px-1 py-3 border-t border-zinc-100/20 hover:bg-zinc-100 hover:text-zinc-900" href="mailto:bastien.winant@gmail.com">hello@figma.com</a>
-				<p className="px-1 py-3 border-t border-zinc-100/20 hover:bg-zinc-100 hover:text-zinc-900">+352 691 223 827</p>
-				<a className="px-1 py-3 border-t border-zinc-100/20 hover:bg-zinc-100 hover:text-zinc-900" href="https://www.xing.com/profile/Bastien_Winant" target="_blank">Xing</a>
-				<a className="px-1 py-3 border-t border-zinc-100/20 hover:bg-zinc-100 hover:text-zinc-900" href="https://www.github.com/BastienWinant" target="_blank">GitHub</a>
+				<a className="px-1 py-3 border-t border-zinc-100/10 hover:bg-zinc-100 hover:text-zinc-900" href="mailto:bastien.winant@gmail.com">hello@figma.com</a>
+				<p className="px-1 py-3 border-t border-zinc-100/10 hover:bg-zinc-100 hover:text-zinc-900">+352 691 223 827</p>
+				<a className="px-1 py-3 border-t border-zinc-100/10 hover:bg-zinc-100 hover:text-zinc-900" href="https://www.xing.com/profile/Bastien_Winant" target="_blank">Xing</a>
+				<a className="px-1 py-3 border-t border-zinc-100/10 hover:bg-zinc-100 hover:text-zinc-900" href="https://www.github.com/BastienWinant" target="_blank">GitHub</a>
 			</nav>
 		</aside>
 	)
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -8,7 +8,7 @@ type ExperienceProps = {
 
 export default function Experience({title, company, dates, location, accomplishments}:ExperienceProps) {
 	return (
-		<div className="flex flex-col gap-y-7 lg:flex-row py-5 border-t border-zinc-100/10 text-sm lg:text-base">
+		<div className="flex flex-col gap-y-7 xl:flex-row xl:justify-between py-5 border-t border-zinc-100/10">
 			<div className="flex flex-col gap-y-3">
 				<h3 className="capitalize font-medium">{title}</h3>
 				<ul className="text-zinc-400">
@@ -17,7 +17,7 @@ export default function Experience({title, company, dates, location, accomplishm
 					<li className="capitalize">{location}</li>
 				</ul>
 			</div>
-			<ul className="list-disc flex flex-col gap-2">
+			<ul className="w-[95%] xl:w-[30em] list-disc flex flex-col gap-2 text-sm xl:text-base">
 				{accomplishments.map(entry => <li>{entry}</li>)}
 			</ul>
 		</div>
