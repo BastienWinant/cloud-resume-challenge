e692719db44a3058ef088ad1692f46f12812a733
BastienWinant <bastien.winant@gmail.com>
2026-01-23 23:54:56 +0100

    SkillsList component

---
+++ src/components/SkillsList.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react"
+
+type SkillsListProps = {
+	children: ReactNode
+
+}
+
+export default function SkillsList({children}:SkillsListProps) {
+	return (
+		<div>
+
+		</div>
+	)
+}
