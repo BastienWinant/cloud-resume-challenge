0933783f8f8882fa73279f48b7abaabf6319a020
BastienWinant <bastien.winant@gmail.com>
2026-02-05 23:57:04 +0100

    project component layout

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,12 +2,26 @@ type ProjectProps = {
 	title: string
 	description: string
 	techstack: string[]
+	githubLink?: string
+	demoLink?: string
 }
 
-export default function Project({title, description, techstack}: ProjectProps) {
+export default function Project({title, description, techstack, githubLink, demoLink}: ProjectProps) {
 	return (
-		<div>
-
-		</div>
+		<article>
+			<header>
+				<h4 className="capitalize">{title}</h4>
+			</header>
+			<div>
+				<p>{description}</p>
+				<ul className="flex items-center">
+					{techstack.map(tech => <li>{tech}</li>)}
+				</ul>
+				<div className="flex items-center">
+					{githubLink && <a href={githubLink}></a>}
+					{demoLink && <a href={demoLink}></a>}
+				</div>
+			</div>
+		</article>
 	)
 }
