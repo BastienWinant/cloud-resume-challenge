975546b7c3777ee0a295b5acac1c6d8e9f2b2cf4
BastienWinant <bastien.winant@gmail.com>
2026-02-05 23:53:00 +0100

    create project component

---
+++ src/components/Project.tsx
@@ -0,0 +1,13 @@
+type ProjectProps = {
+	title: string
+	description: string
+	techstack: string[]
+}
+
+export default function Project({title, description, techstack}: ProjectProps) {
+	return (
+		<div>
+
+		</div>
+	)
+}
